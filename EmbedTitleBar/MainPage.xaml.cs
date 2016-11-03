ef077b6bee38342f6ce180c899e09fb99efba353
liang <liang@DESKTOP-5SV78N4>
2016-11-02 21:20:07 -0700

    Add project files.

---
+++ EmbedTitleBar/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EmbedTitleBar {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonForegroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonHoverForegroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Colors.LightGray;
+            titleBar.ButtonPressedForegroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonPressedForegroundColor = Windows.UI.Colors.Black;
+            titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.Black;
+            Window.Current.SetTitleBar(TitleBar);
+        }
+    }
+}
